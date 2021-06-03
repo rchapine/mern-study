ff6061318419c7266df17fc9944b6f647a5dba03
Rafael Chapine <rchapine@gmail.com>
2021-06-02 22:00:46 -0400

    added backend  review

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import app from "./server.js";
 import mongodb from "mongodb";
 import dotenv from "dotenv";
 import RestaurantsDAO from "./dao/restaurantsDAO.js";
-
+import ReviewsDAO from "./dao/reviewsDAO.js";
 dotenv.config();
 const MongoClient = mongodb.MongoClient;
 
@@ -13,14 +13,13 @@ MongoClient.connect(process.env.RESTREVIEWS_DB_URI, {
   wtimeout: 2500,
   useNewUrlParse: true,
 })
-
   .catch((err) => {
     console.error(err.stack);
     process.exit(1);
   })
-
   .then(async (client) => {
     await RestaurantsDAO.injectDB(client);
+    await ReviewsDAO.injectDB(client);
     app.listen(port, () => {
       console.log(`listening on port ${port}`);
     });
